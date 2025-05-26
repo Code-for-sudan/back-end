d3d0010a893c489b90030b1c0470bf10c230799b
hima890 <hfibrahim90@gmail.com>
2025-05-27 00:10:37 +0200

    Add logging and task for cleaning expired blacklisted tokens in Celery

--- api/api/celery.py
+++ api/api/celery.py
@@ -1,10 +1,12 @@
 """
 Celery configuration file for Django API project.
 """
+import environ, os, logging
 from __future__ import absolute_import, unicode_literals
-import os
 from celery import Celery
-import environ
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken # type: ignore
+from django.utils.timezone import now
+
 
 # Load environment variables from .env file
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,6 +25,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Set the autodiscover to True
 app.autodiscover_tasks()
 
+# Create a logger for Celery tasks
+logger = logging.getLogger('celery')
+
 @app.task(bind=True)
 def debug_task(self):
     """
@@ -40,7 +45,28 @@ def debug_task(self):
         Request: <Context details of the task>
     """
     print(f'Request: {self.request!r}')
+
+@shared_task
+def clean_expired_blacklisted_tokens():
+    """
+    Deletes expired blacklisted tokens from the database.
+    This function queries the BlacklistedToken model for tokens that have expired
+    (i.e., their expiration date is less than the current time) and deletes them.
+    It logs the number of deleted tokens if successful, or logs an error message
+    if an exception occurs during the process.
+    Raises:
+        Exception: If there is an error during the deletion process.
+    """
     
+    try:
+        expired_tokens = BlacklistedToken.objects.filter(token__expires_at__lt=now())
+        count = expired_tokens.count()
+        expired_tokens.delete()
+        logger.info("Deleted {} expired blacklisted tokens.".format(count))
+    except Exception as e:
+        logger.error("Failed to delete expired blacklisted tokens: {}".format(e))
+
+
 # Import periodic tasks for Celery Beat
 from celery.schedules import crontab
 from celery import shared_task
