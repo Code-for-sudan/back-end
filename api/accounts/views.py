583c9288f9ecc7b88e604c62cf2f583e77d13a5d
hima890 <hfibrahim90@gmail.com>
2025-05-29 12:00:59 +0200

    Add throttling to OTP verification endpoint

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -1,10 +1,12 @@
 import logging
-from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes # type: ignore
 from rest_framework.response import Response # type: ignore
 from rest_framework.permissions import AllowAny, IsAuthenticated # type: ignore
 from rest_framework_simplejwt.tokens import RefreshToken # type: ignore
+from rest_framework.decorators import api_view, throttle_classes
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework import status # type: ignore
+from django.shortcuts import render
 from .models import User
 from .utils import generate_jwt_tokens
 
@@ -14,6 +16,7 @@ logger = logging.getLogger('accounts_views')
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
+@throttle_classes([AnonRateThrottle])
 def verify_otp(request):
     """
     Handles OTP verification for user login.
