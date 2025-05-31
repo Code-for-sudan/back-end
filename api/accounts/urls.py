a2e0f72ccc7f5258ea10c7755cb6990ec629a0cc
hima890 <hfibrahim90@gmail.com>
2025-05-31 16:07:49 +0200

    Add sign-up view and URL path for user registration

--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    
+    path('signup/', views.sign_up, name='signup'),
 ]
---
+++ api/accounts/views.py
@@ -0,0 +1,10 @@
+
+import logging
+from rest_framework.decorators import api_view, permission_classes # type: ignore
+from rest_framework.response import Response # type: ignore
+from rest_framework.permissions import AllowAny, IsAuthenticated # type: ignore
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def sign_up(request):
+    pass
