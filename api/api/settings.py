05ddfe739d7bafea13b032c4aab76e4f5d7b450a
hima890 <hfibrahim90@gmail.com>
2025-06-19 20:35:21 +0200

    Update database settings to use MySQL with environment variables

--- api/api/settings.py
+++ api/api/settings.py
@@ -105,8 +105,15 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'myproject'),
+        'USER': os.getenv('DB_USER', 'myuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'mypassword'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'TEST': {
+            'NAME': os.getenv('MYSQL_TEST_DB', 'test_myproject'),
+        },
     }
 }
 
