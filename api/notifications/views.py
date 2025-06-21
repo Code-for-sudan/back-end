d40fe6c0d884f18c37981491e88bd45b947f2b39
hima890 <hfibrahim90@gmail.com>
2025-06-21 20:03:52 +0200

    Refactor email template views to use ModelViewSet for improved functionality and file upload handling

--- api/notifications/views.py
+++ api/notifications/views.py
@@ -1,12 +1,14 @@
 import logging
-from rest_framework.views import APIView
-from rest_framework.response import Response # type: ignore
-from drf_spectacular.utils import extend_schema, OpenApiResponse
-from rest_framework.permissions import AllowAny, IsAuthenticated # type: ignore
-from drf_spectacular.utils import extend_schema
-
+from rest_framework.viewsets import ModelViewSet
+from .models import EmailTemplate
+from .serializers import EmailTemplateSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
 
 # Create a logger for this module
 logger = logging.getLogger('notifications_views')
 
 
+class EmailTemplateViewSet(ModelViewSet):
+    queryset = EmailTemplate.objects.all()
+    serializer_class = EmailTemplateSerializer
+    parser_classes = (MultiPartParser, FormParser)  # Accept form-data (file uploads)
