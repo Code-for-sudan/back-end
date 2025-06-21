42256a024baab551882e0f660b5dbc47b10397a4
hima890 <hfibrahim90@gmail.com>
2025-06-21 20:04:47 +0200

    Add initial URL routing for email templates using DefaultRouter

---
+++ api/notifications/urls.py
@@ -0,0 +1,12 @@
+# urls.py
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import EmailTemplateViewSet
+
+router = DefaultRouter()
+router.register(r'email-templates', EmailTemplateViewSet, basename='email-template')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
