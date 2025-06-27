34d35f48ae1d145641358d7c857fc50978de2d16
hima890 <hfibrahim90@gmail.com>
2025-06-27 23:15:17 +0200

    Remove write-only restriction from otp field in UserSerializer; include phone_number and whatsapp_number in serialized fields

--- api/accounts/serializers.py
+++ api/accounts/serializers.py
@@ -30,8 +30,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     # Set the password field as write-only to prevent it from being serialized
     password = serializers.CharField(write_only=True, min_length=6)
-    # Set otp field as write-only to prevent it from being serialized
-    otp = serializers.CharField(write_only=True, required=False)
     # Set phone_number and whatsapp_number as optional fields
     phone_number = PhoneNumberField(required=False, allow_null=True)
     whatsapp_number = PhoneNumberField(required=False, allow_null=True)
@@ -44,7 +42,9 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'profile_picture',
             'password',
-            'gender'
+            'gender',
+            'phone_number',
+            'whatsapp_number',
         ]
         extra_kwargs = {
             'email': {'required': True},
