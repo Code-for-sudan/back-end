0403f6d5f6f3197200dcac9c15cf9d261e6a7fdc
hima890 <hfibrahim90@gmail.com>
2025-06-28 12:23:33 +0200

    Refactor MySQL initialization script by removing redundant DROP statements

--- api/mysql-init/init.sql
+++ api/mysql-init/init.sql
@@ -1,3 +1,8 @@
+-- This script initializes the MySQL database for the Sudamall project.
+DROP DATABASE IF EXISTS sudamall_db;
+
+DROP DATABASE IF EXISTS test_myproject;
+
 CREATE DATABASE IF NOT EXISTS sudamall_db;
 
 CREATE DATABASE IF NOT EXISTS test_myproject;
@@ -9,4 +14,3 @@ GRANT ALL PRIVILEGES ON sudamall_db.* TO 'api'@'%';
 GRANT ALL PRIVILEGES ON test_myproject.* TO 'api'@'%';
 
 FLUSH PRIVILEGES;
-
