40f0ff37d3a2f1d624c28f3c1f9b6d0a8393552e
hima890 <hfibrahim90@gmail.com>
2025-07-09 11:35:22 +0200

    Fix Google authentication view references in URL patterns

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
-    path('google-auth/', views.GoogleAuthInitView.as_view(), name='google_auth'),
-    path('google-callback/', views.GoogleAuthCallbackView.as_view(), name='google_callback'),
+    path('google-auth/', views.LoginView.as_view(), name='google_auth'),
+    path('google-callback/', views.GoogleCallbackView.as_view(), name='google_callback'),
     path('set-account-type/', views.SetAccountTypeView.as_view(), name='set_account_type'),
     path('seller-setup/', views.SellerSetupView.as_view(), name='seller_setup'),
     path('reset-password/request/', views.PasswordResetRequestView.as_view(), name='reset_password_request'),
