8de180d79113a137fd612849ee5f7a3a71e7f699
hima890 <hfibrahim90@gmail.com>
2025-07-18 10:15:21 +0200

    Fix typo in start_celery.sh and start_celerybeat.sh scripts

--- api/start_celerybeat.sh
+++ api/start_celerybeat.sh
@@ -2,6 +2,5 @@
 set -a
 export $(cat .env | grep -v '^#' | xargs)
 export $(cat .env.prod | grep -v '^#' | xargs)
-celery -A api worker --loglevel=infoariables if needed
 set +a
 celery -A api beat --loglevel=info
