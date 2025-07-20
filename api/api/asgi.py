e8a3cb0e679609434a710857148bc54eb80b41ee
hima890 <hfibrahim90@gmail.com>
2025-07-20 10:28:50 +0200

    Refactor asgi.py to consolidate environment variable setup

--- api/api/asgi.py
+++ api/api/asgi.py
@@ -1,13 +1,15 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
 import django
 django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 import search.routing
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
