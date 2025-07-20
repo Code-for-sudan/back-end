a1b8473a670d82d716a86503e1535354eb20c933
hima890 <hfibrahim90@gmail.com>
2025-07-20 11:13:59 +0200

    Refactor asgi.py to remove redundant environment variable setup

--- api/api/asgi.py
+++ api/api/asgi.py
@@ -3,16 +3,13 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 import django
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from .middleware import JwtAuthMiddleware
 import search.routing
 import chat.routing
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
