7b5a250dc79416b1bab6f8d99095732244e8b5c6
hima890 <hfibrahim90@gmail.com>
2025-07-22 09:54:14 +0000

    hotfix

--- api/api/asgi.py
+++ api/api/asgi.py
@@ -3,6 +3,7 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 import django
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -10,12 +11,9 @@ from .middleware import JwtAuthMiddleware
 import search.routing
 import chat.routing
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-django.setup()
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": JwtAuthMiddleware(
         URLRouter(search.routing.websocket_urlpatterns + chat.routing.websocket_urlpatterns)),
 })
+
