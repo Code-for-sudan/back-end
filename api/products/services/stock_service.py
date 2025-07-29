f4708e9a02ed185ddf0b062c72518e2d3e6330a8
Omer Yassir <omerym10@gmail.com>
2025-07-29 05:49:39 +0300

    stock management tests

--- api/products/services/stock_service.py
+++ api/products/services/stock_service.py
@@ -3,65 +3,72 @@ import logging
 from django.db import transaction
 from django.core.exceptions import ValidationError
 from typing import Union
-from api.products.models import Product, Size
+from products.models import Product, Size
 
 logger = logging.getLogger("stock_service")
+
+
 class StockService:
 
     @staticmethod
     @transaction.atomic
-
     def reserve_stock(product_id, quantity, size=None) -> Union[Product, Size]:
         product = Product.objects.select_for_update().get(pk=product_id)
 
         if product.has_sizes:
             if not size:
                 logger.error("Size must be specified for products with sizes.")
-                raise ValidationError("Size must be specified for products with sizes.")
+                raise ValidationError(
+                    "Size must be specified for products with sizes.")
             size_obj = Size.objects.select_for_update().get(product=product, size=size)
             if size_obj.available_quantity < quantity:
-                logger.error("Not enough stock available for size %s of product %s.", size, product_id)
-                raise ValidationError("Not enough stock available for this size.")
+                logger.error(
+                    "Not enough stock available for size %s of product %s.", size, product_id)
+                raise ValidationError(
+                    "Not enough stock available for this size.")
             size_obj.available_quantity -= quantity
             size_obj.reserved_quantity += quantity
             size_obj.save()
             return size_obj
 
         if product.available_quantity < quantity:
-            logger.error("Not enough stock available for product %s.", product_id)
+            logger.error(
+                "Not enough stock available for product %s.", product_id)
             raise ValidationError("Not enough stock available.")
         product.available_quantity -= quantity
         product.reserved_quantity += quantity
         product.save()
         return product
 
-@staticmethod
-@transaction.atomic
-def unreserve_stock(product_id, quantity, size=None):
-    product = Product.objects.select_for_update().get(pk=product_id)
+    @staticmethod
+    @transaction.atomic
+    def unreserve_stock(product_id, quantity, size=None, returned=True) -> Union[Product, Size]:
+        product = Product.objects.select_for_update().get(pk=product_id)
 
-    if product.has_sizes:
-        if not size:
-            logger.error("Size must be specified for products with sizes.")
-            raise ValidationError("Size must be specified for products with sizes.")
-        size_obj = Size.objects.select_for_update().get(product=product, size=size)
-        size_obj.available_quantity += quantity
-        size_obj.reserved_quantity -= quantity
-        if size_obj.reserved_quantity < 0:
+        if product.has_sizes:
+            if not size:
+                logger.error("Size must be specified for products with sizes.")
+                raise ValidationError(
+                    "Size must be specified for products with sizes.")
+            size_obj = Size.objects.select_for_update().get(product=product, size=size)
+            if returned:
+                size_obj.available_quantity += quantity
+            size_obj.reserved_quantity -= quantity
+            if size_obj.reserved_quantity < 0:
+                logger.warning(
+                    "Reserved quantity for size %s of product %s went negative (adjusting to 0).",
+                    size, product_id
+                )
+                size_obj.reserved_quantity = 0
+            size_obj.save()
+            return size_obj
+        if returned:
+            product.available_quantity += quantity
+        product.reserved_quantity -= quantity
+        if product.reserved_quantity < 0:
             logger.warning(
-                "Reserved quantity for size %s of product %s went negative (adjusting to 0).",
-                size, product_id
+                "Reserved quantity for product %s went negative (adjusting to 0).", product_id
             )
-            size_obj.reserved_quantity = 0
-        size_obj.save()
-        return size_obj
-
-    product.available_quantity += quantity
-    product.reserved_quantity -= quantity
-    if product.reserved_quantity < 0:
-        logger.warning(
-            "Reserved quantity for product %s went negative (adjusting to 0).", product_id
-        )
-        product.reserved_quantity = 0
-    product.save()
-    return product
+            product.reserved_quantity = 0
+        product.save()
+        return product
--- api/products/urls.py
+++ api/products/urls.py
@@ -1,10 +1,12 @@
-from .views import ProductViewSet
-from django.urls import path, include
+from .views import DeleteProductSizeView, ProductViewSet
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'', ProductViewSet, basename='product')
+router.register(r'', ProductViewSet, basename='product',)
 
-urlpatterns = []
+urlpatterns = [path('<int:product_id>/sizes/<int:size_id>/delete/',
+                    DeleteProductSizeView.as_view(),
+                    name='delete-product-size'),]
 
 urlpatterns += router.urls
