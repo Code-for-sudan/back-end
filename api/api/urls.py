a1b1f560076b72f606ba59d490c2697c15ac2c5e
hima890 <hfibrahim90@gmail.com>
2025-08-12 16:51:29 +0200

    Replace TokenObtainPairView with CookieTokenObtainPairView for enhanced JWT security

--- api/api/urls.py
+++ api/api/urls.py
@@ -26,10 +26,12 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView
 )
+from authentication.views import CookieTokenObtainPairView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
